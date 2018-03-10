3f36305d6023e87abae278a78a0697fa2e7fb937
Jose Felix Longares Moreno <696215@unizar.es>
2018-03-10 20:28:06 +0100

    Creada PlayerActivity v1

--- app/mobile/src/main/java/android/prosotec/proyectocierzo/PlayerActivity.kt
+++ app/mobile/src/main/java/android/prosotec/proyectocierzo/PlayerActivity.kt
@@ -2,12 +2,11 @@ package android.prosotec.proyectocierzo
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.android.synthetic.main.activity_login.*
 
-class LoginActivity : AppCompatActivity() {
+class PlayerActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(R.layout.activity_player)
     }
 }
