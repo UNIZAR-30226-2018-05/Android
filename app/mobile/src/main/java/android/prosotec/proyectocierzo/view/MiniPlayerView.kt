9ab0e4b5ad2bbeb51092334e9dcc80b88429488c
Roberto <720100@unizar.es>
2018-03-18 23:57:54 +0100

    Cambiados los colores y añadida Toolbar (todavía sin funcionalidad)

--- app/mobile/src/main/java/android/prosotec/proyectocierzo/view/MiniPlayerView.kt
+++ app/mobile/src/main/java/android/prosotec/proyectocierzo/view/MiniPlayerView.kt
@@ -4,7 +4,10 @@ import android.content.Context
 import android.prosotec.proyectocierzo.R
 import android.util.AttributeSet
 import android.view.LayoutInflater
+import android.widget.ImageButton
+import android.widget.ImageView
 import android.widget.RelativeLayout
+import android.widget.TextView
 
 /**
  * Created by ccucr on 18/03/2018.
@@ -18,5 +21,14 @@ class MiniPlayerView @JvmOverloads constructor(
 
     init {
         LayoutInflater.from(context).inflate(R.layout.view_new_mini_player,this,true)
+        var title = findViewById<TextView>(R.id.title)
+        var author = findViewById<TextView>(R.id.author)
+        var length = findViewById<TextView>(R.id.length)
+        var actual_time = findViewById<TextView>(R.id.actual_time)
+        var cover = findViewById<ImageView>(R.id.cover_image)
+        var skip_prev_button = findViewById<ImageButton>(R.id.skip_prev)
+        var skip_next_button = findViewById<ImageButton>(R.id.skip_next)
+        var play_button = findViewById<ImageButton>(R.id.play)
     }
+
 }
