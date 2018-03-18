17ad4f62dc0b26c2768ef38254cebc7bee32c8ee
Roberto <720100@unizar.es>
2018-03-18 19:44:18 +0100

    Creada View para el mini-reproductor y que servirá de ejemplo.

---
+++ app/mobile/src/main/java/android/prosotec/proyectocierzo/MainActivity.kt
@@ -0,0 +1,18 @@
+package android.prosotec.proyectocierzo
+
+
+import android.app.Activity
+import android.os.Bundle
+
+/**
+ * Created by ccucr on 18/03/2018.
+ */
+class MainActivity : Activity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+
+}
