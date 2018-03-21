fe37b952b78b414918e934df6c9584d0ed296781
José Félix Longares Moreno <696215@unizar.es>
2018-03-21 23:23:34 +0100

    Actualizado icono a vector

--- app/mobile/src/main/java/android/prosotec/proyectocierzo/IndexActivity.kt
+++ app/mobile/src/main/java/android/prosotec/proyectocierzo/IndexActivity.kt
@@ -4,6 +4,7 @@ import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.design.widget.NavigationView
 import android.support.v4.view.GravityCompat
+import android.support.v4.widget.DrawerLayout.LOCK_MODE_LOCKED_CLOSED
 import android.support.v7.app.ActionBarDrawerToggle
 import android.support.v7.app.AppCompatActivity
 import android.view.Menu
@@ -19,9 +20,9 @@ class IndexActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelect
         setContentView(R.layout.activity_index)
         setSupportActionBar(toolbar)
 
-
         val toggle = ActionBarDrawerToggle(
                 this, drawer_layout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close)
+
         drawer_layout.addDrawerListener(toggle)
         toggle.syncState()
 
